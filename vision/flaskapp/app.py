9f8aa7c9ddb9ca70917d08368c0f84219030ef20
Henry Deutsch <henrymdeutsch@gmail.com>
2022-09-18 03:43:08 -0400

    added vision folder with non-website stuff

---
+++ vision/flaskapp/app.py
@@ -0,0 +1,149 @@
+import os
+from flask import Flask, render_template, request, Response
+import cv2
+import io
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+from six import BytesIO
+from PIL import Image
+from six.moves.urllib.request import urlopen
+
+import tensorflow as tf
+import tensorflow_hub as hub
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+
+def load_image_into_numpy_array(image):
+  """Load an image from file into a numpy array.
+
+  Puts image into numpy array to feed into tensorflow graph.
+  Note that by convention we put it into a numpy array with shape
+  (height, width, channels), where channels=3 for RGB.
+
+  Args:
+    path: the file path to the image
+
+  Returns:
+    uint8 numpy array with shape (img_height, img_width, 3)
+  """
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (1, im_height, im_width, 3)).astype(np.uint8)
+ALL_MODELS = {
+'CenterNet HourGlass104 512x512' : 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1',
+'CenterNet HourGlass104 Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512_kpts/1',
+'CenterNet HourGlass104 1024x1024' : 'https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024/1',
+'CenterNet HourGlass104 Keypoints 1024x1024' : 'https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024_kpts/1',
+'CenterNet Resnet50 V1 FPN 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512/1',
+'CenterNet Resnet50 V1 FPN Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512_kpts/1',
+'CenterNet Resnet101 V1 FPN 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet101v1_fpn_512x512/1',
+'CenterNet Resnet50 V2 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512/1',
+'CenterNet Resnet50 V2 Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512_kpts/1',
+'EfficientDet D0 512x512' : 'https://tfhub.dev/tensorflow/efficientdet/d0/1',
+'EfficientDet D1 640x640' : 'https://tfhub.dev/tensorflow/efficientdet/d1/1',
+'EfficientDet D2 768x768' : 'https://tfhub.dev/tensorflow/efficientdet/d2/1',
+'EfficientDet D3 896x896' : 'https://tfhub.dev/tensorflow/efficientdet/d3/1',
+'EfficientDet D4 1024x1024' : 'https://tfhub.dev/tensorflow/efficientdet/d4/1',
+'EfficientDet D5 1280x1280' : 'https://tfhub.dev/tensorflow/efficientdet/d5/1',
+'EfficientDet D6 1280x1280' : 'https://tfhub.dev/tensorflow/efficientdet/d6/1',
+'EfficientDet D7 1536x1536' : 'https://tfhub.dev/tensorflow/efficientdet/d7/1',
+'SSD MobileNet v2 320x320' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2',
+'SSD MobileNet V1 FPN 640x640' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v1/fpn_640x640/1',
+'SSD MobileNet V2 FPNLite 320x320' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_320x320/1',
+'SSD MobileNet V2 FPNLite 640x640' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_640x640/1',
+'SSD ResNet50 V1 FPN 640x640 (RetinaNet50)' : 'https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_640x640/1',
+'SSD ResNet50 V1 FPN 1024x1024 (RetinaNet50)' : 'https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_1024x1024/1',
+'SSD ResNet101 V1 FPN 640x640 (RetinaNet101)' : 'https://tfhub.dev/tensorflow/retinanet/resnet101_v1_fpn_640x640/1',
+'SSD ResNet101 V1 FPN 1024x1024 (RetinaNet101)' : 'https://tfhub.dev/tensorflow/retinanet/resnet101_v1_fpn_1024x1024/1',
+'SSD ResNet152 V1 FPN 640x640 (RetinaNet152)' : 'https://tfhub.dev/tensorflow/retinanet/resnet152_v1_fpn_640x640/1',
+'SSD ResNet152 V1 FPN 1024x1024 (RetinaNet152)' : 'https://tfhub.dev/tensorflow/retinanet/resnet152_v1_fpn_1024x1024/1',
+'Faster R-CNN ResNet50 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_640x640/1',
+'Faster R-CNN ResNet50 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_1024x1024/1',
+'Faster R-CNN ResNet50 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_800x1333/1',
+'Faster R-CNN ResNet101 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_640x640/1',
+'Faster R-CNN ResNet101 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_1024x1024/1',
+'Faster R-CNN ResNet101 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_800x1333/1',
+'Faster R-CNN ResNet152 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_640x640/1',
+'Faster R-CNN ResNet152 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_1024x1024/1',
+'Faster R-CNN ResNet152 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_800x1333/1',
+'Faster R-CNN Inception ResNet V2 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_640x640/1',
+'Faster R-CNN Inception ResNet V2 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_1024x1024/1',
+'Mask R-CNN Inception ResNet V2 1024x1024' : 'https://tfhub.dev/tensorflow/mask_rcnn/inception_resnet_v2_1024x1024/1'
+}
+PATH_TO_LABELS = '../models/research/object_detection/data/mscoco_label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+print('loading model...')
+global hub_model
+model_handle = "SSD MobileNet v2 320x320"
+hub_model = hub.load(ALL_MODELS[model_handle])
+print('model loaded!')
+def pred(image_np):
+    global vals
+    # image_path = "sample_img/apple.jpg"
+    results = hub_model(image_np)
+    result = {key:value.numpy() for key,value in results.items()}
+    label_id_offset = 0
+    image_np_with_detections = image_np.copy()
+    res, vals = viz_utils.visualize_boxes_and_labels_on_image_array(
+      image_np_with_detections[0],
+      result['detection_boxes'][0],
+      (result['detection_classes'][0] + label_id_offset).astype(int),
+      result['detection_scores'][0],
+      category_index,
+      use_normalized_coordinates=True,
+      max_boxes_to_draw=3,
+      min_score_thresh=0.3,
+      agnostic_mode=False)
+    return image_np_with_detections[0]
+app = Flask(__name__, static_folder="static", static_url_path="/static", template_folder="templates")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+def generate_entry(item, weight):
+  price = {"apple":1.31, "banana":.43, "broccoli":1.99, "carrot":1.5, "orange":.99}
+  return f"<p>{item}, {weight}g</p><p>${round(weight*price[item]/250,2)}</p><hr/>"
+
+@app.route('/add-to-cart')
+def add_to_cart():
+  prev = request.args.get('content')
+  print(vals)
+  if(len(vals) > 0):
+    for key, val in vals.items():
+      prev+=generate_entry(key,val)
+  return prev
+global frame, vals
+capture=0
+switch=1
+camera_id = 1
+camera = cv2.VideoCapture(camera_id)
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        success, frame = camera.read()
+        if success:
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
+                frame = buffer.tobytes()
+
+                img = Image.open(io.BytesIO(frame))
+
+                img_np = load_image_into_numpy_array(img)
+                results = pred(img_np)
+                frame = cv2.imencode('.jpg', results[...,::-1])[1].tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                print(e)
+                pass
+
+        else:
+            pass
+
+if __name__ == '__main__':
+    app.run()
